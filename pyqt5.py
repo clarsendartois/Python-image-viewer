03d6b931f20cc972b28621efd4999f18fae4eefc
Clarsen D'Artois <dartoisclarsen91@gmail.com>
2023-04-10 22:45:47 -0400

    Edit

--- pyqt5.py
+++ pyqt5.py
@@ -3,19 +3,39 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 00)
+        self.setWindowTitle("Tech Wiht Tim!")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        
+        self.label.setText("My first label!")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("You pressed the button")
+        self.update()
+
+    def update(sefl):
+        sefl.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(0, 0, 300, 300)
+    win = MyWindow()
+    win.setGeometry(200, 200, 300, 00)
     win.setWindowTitle("Tech Wiht Tim!")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("My first label!")
-    label.move(50, 50)
 
     win.show()
     sys.exit(app.exec_())
 
 
 window()
-# 2.PyQt5 Tutorial - Buttons and Events (Signals)
